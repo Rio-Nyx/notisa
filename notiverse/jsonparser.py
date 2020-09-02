2115f0e296a5f784df1a276139cd6a436b340cb5
Rio_Nyx <58178550+Rio-Nyx@users.noreply.github.com>
2020-09-02 10:49:44 +0530

    removed unwanted functions

--- notiverse/jsonparser.py
+++ notiverse/jsonparser.py
@@ -1,17 +1,9 @@
 import json
-def jsonload(key,value):
-	with open("config.json", "r") as files:
-		json_data = json.load(files)
-		print(json_data[key])
-		json_data[key]=value				
-	with open('config.json', 'w') as file:
-		json.dump(json_data, file, indent=2)
+
 def jsonread():
 	with open("config.json", "r") as files:
 		json_data = json.load(files)
 	return json_data
 def jsonchange(settings):
 	with open('config.json', 'w') as files:			
-		#json.dump(settings, file, indent=2)
 		json.dump(settings, files)
-#jsonload("background","red")
